303a92788ea031d892108a25928b92e99da39bfc
hzy <hezhengyu.hzy@gmail.com>
2026-01-06 15:18:16 +0800

    modified:   yunikorn/config/yunikorn-configs.yaml 	new file:   yunikorn/k8s-job/commands.sh 	new file:   yunikorn/k8s-job/test-job-simple.yaml

---
+++ yunikorn/k8s-job/commands.sh
@@ -0,0 +1,22 @@
+
+export QUEUE=root.ns-a.tenant-a
+export TIMESTAMP=$(date +%s)
+export COMPLETIONS=1000
+export PARALLELISM=16
+export SLEEP_DURATION=600
+export MIN_MEMORY="1024Mi"
+export MIN_CPU="200m"
+export MAX_MEMORY="1024Mi"
+export MAX_CPU="200m"
+envsubst < test-job-simple.yaml | kubectl apply -f -;
+sleep 5;
+export QUEUE=root.ns-a.tenant-b
+export TIMESTAMP=$(date +%s)
+export COMPLETIONS=100
+export PARALLELISM=4
+export SLEEP_DURATION=600
+export MIN_MEMORY="1024Mi"
+export MIN_CPU="200m"
+export MAX_MEMORY="1024Mi"
+export MAX_CPU="200m"
+envsubst < test-job-simple.yaml | kubectl apply -f -;
