1eb9d62c0522267f611bb44f60e686e0942c6674
hzy <hezhengyu.hzy@gmail.com>
2026-01-06 16:56:11 +0800

    modified:   yunikorn/config/yunikorn-configs.yaml 	modified:   yunikorn/k8s-job/commands.sh 	modified:   yunikorn/k8s-job/test-job-simple.yaml

--- yunikorn/k8s-job/commands.sh
+++ yunikorn/k8s-job/commands.sh
@@ -2,7 +2,7 @@
 export QUEUE=root.ns-a.tenant-a
 export TIMESTAMP=$(date +%s)
 export COMPLETIONS=1000
-export PARALLELISM=16
+export PARALLELISM=12
 export SLEEP_DURATION=600
 export MIN_MEMORY="1024Mi"
 export MIN_CPU="200m"
@@ -13,7 +13,7 @@ sleep 5;
 export QUEUE=root.ns-a.tenant-b
 export TIMESTAMP=$(date +%s)
 export COMPLETIONS=100
-export PARALLELISM=4
+export PARALLELISM=12
 export SLEEP_DURATION=600
 export MIN_MEMORY="1024Mi"
 export MIN_CPU="200m"
