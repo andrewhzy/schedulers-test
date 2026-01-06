5a53207e5a5c4a7de6d5b25dc1b8fac2b0d31484
hzy <hezhengyu.hzy@gmail.com>
2026-01-06 15:27:30 +0800

    new file:   yunikorn/spark/spark-submit/job.yaml 	new file:   yunikorn/spark/spark-submit/test-job-simple.yaml 	new file:   yunikorn/spark/spark-submit/test-job-standalone.yaml 	new file:   yunikorn/spark/spark-submit/test-job-workflow.yaml 	new file:   yunikorn/spark/spark-submit/test.sh

---
+++ yunikorn/spark/spark-submit/test.sh
@@ -0,0 +1,22 @@
+
+export QUEUE=root.ns-a.tenant-low-priority
+export TIMESTAMP=$(date +%s)
+export COMPLETIONS=1000
+export PARALLELISM=12
+export SLEEP_DURATION=600
+export MIN_MEMORY="1024Mi"
+export MIN_CPU="200m"
+export MAX_MEMORY="1024Mi"
+export MAX_CPU="200m"
+envsubst < test-job-simple.yaml | kubectl apply -f -
+
+export QUEUE=root.ns-a.tenant-normal-priority
+export TIMESTAMP=$(date +%s)
+export COMPLETIONS=1000
+export PARALLELISM=12
+export SLEEP_DURATION=600
+export MIN_MEMORY="1024Mi"
+export MIN_CPU="200m"
+export MAX_MEMORY="1024Mi"
+export MAX_CPU="200m"
+envsubst < test-job-simple.yaml | kubectl apply -f -
